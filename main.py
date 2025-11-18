90378fd1310a29f197005a62552904517657faa8
KohlGoldsmith <96486364+KohlGoldsmith@users.noreply.github.com>
2025-11-18 14:31:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-#Main file that runs all four utils to fetch the latest data from FAERS and regulate it for the model
+#Main file that runs all  utils to fetch the latest data from FAERS and regulate it for the model
 
 from utils.latest_data_util import get_latest_faers_release   # if you put it in utils/latest_checker.py
 from utils.faers_data_utils import fetch_and_extract_faers_xml
 from utils.data_regulator_utils import regulate_data
 from utils.csv_reader_utils import csv_reader
 from utils.combiner_utils import combine_with_embedding
+from utils.ml_grouper_utils import flatten_grouped_lists
+
 
 def main():
     print ("Checking latest FAERS data release...")
@@ -14,7 +16,7 @@ def main():
     else:
         print("\nCould not detect latest FAERS release.")
 
-    print("\nPlease enter which dataset you want to use:")
+    print("\nPlease enter which dataset you want to use.")
     year = int(input("Enter FAERS Year: "))
     quarter = int(input("Enter Quarter (1–4): "))
 
@@ -22,20 +24,23 @@ def main():
     extracted_dir = fetch_and_extract_faers_xml(year, quarter)
 
     # STEP 2
-    cleaned_csv = regulate_data(extracted_dir, "dataset/faers_cleaned")
-
+    cleaned_csv = regulate_data(extract_dir=extracted_dir, output_dir="dataset/faers_data/cleaned_data")
     # STEP 3
-    flattened_df = csv_reader(cleaned_csv)
+    #flattened_df, flattened_csv_path = csv_reader(cleaned_csv)
 
     # STEP 4
     final_csv = combine_with_embedding(
-        flattened_df,
+        cleaned_csv,
         "dataset/dictionary/Side_effects_unique.csv",
         "dataset/processed"
     )
-
+    # STEP 5
+    final_flattened_csv = flatten_grouped_lists(
+        grouped_csv_path=final_csv,
+        output_dir="dataset/faers_data/cleaned_data"
+    )
     print("\nPipeline complete and data is prepared.")
-    print("Final ML file:", final_csv)
+    print("Final ML file:", final_flattened_csv)
 
 if __name__ == "__main__":
     main()
