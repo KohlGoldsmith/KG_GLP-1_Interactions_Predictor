98036ce019efbbcc6d9ecaf5c2abb72760a01027
KohlGoldsmith <96486364+KohlGoldsmith@users.noreply.github.com>
2025-11-19 11:21:16 -0500

    Fix formatting of comment in main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-#Main file that runs all  utils to fetch the latest data from FAERS and regulate it for the model
+# Main file that runs all  utils to fetch the latest data from FAERS and regulate it for the model
 
-from utils.latest_data_util import get_latest_faers_release   # if you put it in utils/latest_checker.py
+from utils.latest_data_util import get_latest_faers_release 
 from utils.faers_data_utils import fetch_and_extract_faers_xml
 from utils.data_regulator_utils import regulate_data
 from utils.csv_reader_utils import csv_reader
