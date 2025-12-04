e51b67a474c641ab46f41643f89ced0b937e9124
KohlGoldsmith <96486364+KohlGoldsmith@users.noreply.github.com>
2025-12-04 12:51:30 -0500

    Remove docstring from get_latest_faers_release
    Removed docstring from get_latest_faers_release function.

--- utils/latest_data_util.py
+++ utils/latest_data_util.py
@@ -6,10 +6,6 @@ from datetime import datetime
 BASE_URL="https://fis.fda.gov/content/Exports/"
 
 def get_latest_faers_release(max_year_back=15):
-    """
-    Detect the newest available FAERS XML release by checking URLs backward
-    from the current year/quarter.
-    """
     now = datetime.now()
     current_year = now.year
     current_quarter = (now.month - 1) // 3 + 1
